c32061f55afaf9bc7451d6a511829c410bdff446
Prinye <71138917+wilc0060@users.noreply.github.com>
2023-03-19 23:27:28 -0400

    done

---
+++ main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("Links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+// myFunction()
